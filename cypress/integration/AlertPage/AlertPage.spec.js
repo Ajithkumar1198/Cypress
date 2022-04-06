5a93e6f49e9c7eea6fa7ff23e597de985876d555
Ajith <ajithkumar@annulartechnologies.com>
2022-04-06 18:30:57 +0530

    check

--- cypress/integration/AlertPage/AlertPage.spec.js
+++ cypress/integration/AlertPage/AlertPage.spec.js
@@ -3,6 +3,7 @@ import { Given,Then,And} from "cypress-cucumber-preprocessor/steps";
 Given('open Testleaf url',()=>{
     //const h=new ItAutomationPage()
     //yvyvygvygy
+    //repocheck
      ItAutomationPage.visit()
 })
 Then('click the alert option',()=>
